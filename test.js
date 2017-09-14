636e9c3ad3be17369094ab6cb9e7e34d0622526f
Godfey <hiyangguo@qq.com>
2017-09-14 22:51:21 +0800

    update:rewrite build script

--- test.js
+++ test.js
@@ -3,16 +3,23 @@
  */
 var rsuiteCssBuild = require('./main');
 
-rsuiteCssBuild.importResources({
-    needDirPath: true,
-    paths: [
-        'fonts/**/*.*'
-    ],
-    dist: 'dist/test'
-});
+const palette = () => {
+    rsuiteCssBuild.palette({
+        baseColor: '#1b9451',
+        src: 'css/rsuite.min.css',
+        dist: 'dist/test/css/rsuite.min.css'
+    });
+};
 
-rsuiteCssBuild.palette({
-    baseColor: '#1b9451',
-    src: 'css/rsuite.min.css',
-    dist: 'dist/test/css/rsuite.min.css'
-});
+const importResources = (callback) => {
+    rsuiteCssBuild.importResources({
+        needDirPath: true,
+        paths: [
+            'fonts/**/*.*',
+            'css/**/*'
+        ],
+        dist: 'dist/test'
+    }, callback);
+};
+
+importResources(palette);
